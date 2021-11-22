17c9f9b12873aa42e13438abf64e1de6cada710e
Ian Haken <ihaken@netflix.com>
2021-11-21 17:27:22 -0800

    Initial commit.

---
+++ src/index.tsx
@@ -0,0 +1,112 @@
+import {Component, createElement, ReactNode} from 'react';
+import {render} from "react-dom";
+import {Button, Container, Form, Grid, Header, Icon, Input, Segment} from "semantic-ui-react";
+import {CanvasRenderer, ImageElement, ReactRenderer, RenderElement, TextElement} from "./renderer";
+
+interface Props {
+}
+
+interface State {
+  website?: string
+  program_info?: string
+  logo?: string
+  pic?: string
+}
+
+class Demo extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      website: "",
+      program_info: "",
+    };
+  }
+
+  private setImage(fieldName: 'logo' | 'pic', files: FileList) {
+    if (!files || files.length === 0) {
+      this.setState({[fieldName]: undefined});
+      return;
+    }
+    let self = this;
+    let file = files[0];
+    if (file.type && file.type.indexOf('image/') !== 0) {
+      alert("File is not an image: " + file.type);
+      return;
+    }
+    let reader = new FileReader();
+    reader.addEventListener('load', function (e) {
+      self.setState({[fieldName]: e.target.result})
+    });
+    reader.readAsDataURL(file);
+  }
+
+  private downloadPng() {
+    let canvasPromise = new CanvasRenderer().render(this.getRenderChildren());
+    canvasPromise.then(canvas => {
+      let image = canvas.toDataURL("image/png");
+      let link = document.createElement("a");
+      link.download = 'foo.png';
+      link.href = image;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }).catch(err => {
+      alert(err);
+    });
+  }
+
+  private getRenderChildren(): Array<RenderElement> {
+    return [
+      new ImageElement("bg-thumbnail.jpg", {x: 0, y: 0, width: 1571, height: 2000}),
+      new ImageElement(this.state.logo, {x: 1271, y: 1453, width: 300, height: 300, bgFill: '#ffbd59'}),
+      new ImageElement(this.state.pic, {x: 0, y: 318, width: 1571, height: 1435}),
+      new TextElement(this.state.website, {x: 1571, y: 75, textAnchor: 'end', fontSize: 75, fontWeight: 'bold', fontFamily: 'Quicksand, sans-serify', bgFill: '#ffbd59', bgPadding: 5}),
+      new TextElement(this.state.program_info, {x: 785, y: 1970, textAnchor: 'middle', fontSize: 90, color: 'white', fontWeight: 'bold', fontFamily: 'Quicksand, sans-serify'})
+    ];
+  }
+
+  private renderToDom(): ReactNode {
+    return new ReactRenderer().render(this.getRenderChildren());
+  }
+
+  render() {
+    return <Container>
+      <Header as="h1">
+        RVCC Demo
+      </Header>
+      <Segment>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Form>
+                <Form.Field>
+                  <label>Website / Social Media</label>
+                  <Input value={this.state.website} onChange={e => this.setState({website: e.target.value})} />
+                </Form.Field>
+                <Form.Field>
+                  <label>Program Information</label>
+                  <Input value={this.state.program_info} onChange={e => this.setState({program_info: e.target.value})} />
+                </Form.Field>
+                <Form.Field>
+                  <label>Logo</label>
+                  <Input type="file" onChange={e => this.setImage('logo', e.target.files)} />
+                </Form.Field>
+                <Form.Field>
+                  <label>Picture</label>
+                  <Input type="file" onChange={e => this.setImage('pic', e.target.files)} />
+                </Form.Field>
+                <Button primary icon labelPosition="left" onClick={() => this.downloadPng()}><Icon name="download" />Download (PNG)</Button>
+              </Form>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Header as="h2">Preview</Header>
+              {this.renderToDom()}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </Container>
+  }
+}
+
+render(createElement(Demo), document.getElementById('demo'));
---
+++ src/renderer.tsx
@@ -0,0 +1,252 @@
+import {Component, createRef, ReactNode, Fragment, ReactSVGElement, Ref, RefObject, SVGProps} from "react";
+
+export interface RenderElement {
+    renderToReact(key: any): ReactNode;
+    renderToCanvas(ctx: CanvasRenderingContext2D): Promise<void>;
+}
+
+export interface TextElementOptions {
+    x: number;
+    y: number;
+    textAnchor?: 'start' | 'end' | 'middle';
+    fontSize?: number;
+    color?: string,
+    fontWeight?: 'bold';
+    fontFamily?: string;
+    bgFill?: string;
+    bgPadding?: number;
+}
+
+interface BgSvgTextNodeProps {
+    text: string;
+    textProps: SVGProps<SVGTextElement>;
+    bgFill?: string;
+    bgPadding?: number;
+}
+
+class BgSvgTextNode extends Component<BgSvgTextNodeProps, {extents?: DOMRect}> {
+    private ref: RefObject<SVGTextElement>;
+
+    constructor(props: BgSvgTextNodeProps) {
+        super(props);
+        this.ref = createRef();
+        this.state = {};
+    }
+
+    componentDidMount() {
+        this.updateExtents();
+    }
+
+    componentDidUpdate(prevProps: BgSvgTextNodeProps) {
+        if (prevProps.text !== this.props.text) {
+            this.updateExtents();
+        }
+    }
+
+    private updateExtents() {
+        this.setState({extents: this.ref.current.getBBox()});
+    }
+
+    render() {
+        const margin = this.props.bgPadding || 0;
+        const extents = this.state.extents;
+
+        let outline: ReactNode;
+        if (this.props.bgFill && extents) {
+            outline = <rect fill={this.props.bgFill} x={extents.x - margin} y = {extents.y - margin } width={extents.width + 2 * margin} height={extents.height + 2 * margin} />;
+        }
+        return <Fragment>{outline}<text ref={this.ref} {...this.props.textProps}>{this.props.text}</text></Fragment>;
+    }
+}
+
+export class TextElement implements RenderElement {
+    private text: string
+    private options: TextElementOptions;
+
+    constructor(text: string, options: TextElementOptions) {
+        this.text = text;
+        this.options = options;
+    }
+
+    renderToReact(key: any): React.ReactNode {
+        if (!this.text) {
+            return null;
+        }
+
+        return <BgSvgTextNode key={key} text={this.text} textProps={{
+            x: this.options.x,
+            y: this.options.y,
+            textAnchor: this.options.textAnchor,
+            fontSize: this.options.fontSize,
+            fill: this.options.color,
+            fontWeight: this.options.fontWeight,
+            fontFamily: this.options.fontFamily,
+        }} bgFill={this.options.bgFill} bgPadding={this.options.bgPadding} />;
+    }
+
+    renderToCanvas(ctx: CanvasRenderingContext2D): Promise<void> {
+        if (this.options.textAnchor === 'middle') {
+            ctx.textAlign = 'center';
+        } else if (this.options.textAnchor === 'end') {
+            ctx.textAlign = 'right';
+        } else {
+            ctx.textAlign = 'left';
+        }
+
+        let font: string = '';
+        if (this.options.fontWeight) {
+            font += this.options.fontWeight + ' ';
+        }
+        if (this.options.fontSize) {
+            font += this.options.fontSize + 'px ';
+        }
+        if (this.options.fontFamily) {
+            font += this.options.fontFamily;
+        }
+        if (font) {
+            ctx.font = font;
+        }
+
+        if (this.options.bgFill) {
+            let metrics = ctx.measureText(this.text);
+            let height = this.options.fontSize || 0;
+            let width = metrics.width;
+            let x = this.options.x;
+            let y = this.options.y - height;
+            if (this.options.textAnchor === 'middle') {
+                x -= width/2;
+            } else if (this.options.textAnchor === 'end') {
+                x -= width;
+            }
+
+            ctx.fillStyle = this.options.bgFill;
+            ctx.fillRect(x, y, width, height);
+        }
+
+        if (this.options.color) {
+            ctx.fillStyle = this.options.color;
+        } else {
+            ctx.fillStyle = 'black';
+        }
+        ctx.fillText(this.text, this.options.x, this.options.y);
+
+        return Promise.resolve();
+    }
+}
+
+export interface ImageElementOptions {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+
+    bgFill?: string
+    bgPadding?: number;
+}
+
+export class ImageElement implements RenderElement {
+    private href: string;
+    private options: ImageElementOptions;
+
+    constructor(href: string, options: ImageElementOptions) {
+        this.href = href;
+        this.options = options;
+    }
+
+    renderToReact(key: any): React.ReactNode {
+        if (!this.href) {
+            return null;
+        }
+
+        let img = <image
+            key={key}
+            href={this.href}
+            x={this.options.x}
+            y={this.options.y}
+            width={this.options.width}
+            height={this.options.height}>
+        </image>;
+
+        if (this.options.bgFill) {
+            const bgPadding = this.options.bgPadding || 0;
+            return <Fragment>
+                <rect x={this.options.x - bgPadding}
+                      y={this.options.y - bgPadding}
+                      width={this.options.width + 2*bgPadding}
+                      height={this.options.height + 2*bgPadding}
+                      fill={this.options.bgFill} />
+                {img}
+            </Fragment>
+        }
+
+        return img;
+    }
+
+    renderToCanvas(ctx: CanvasRenderingContext2D): Promise<void> {
+        if (!this.href) {
+            return Promise.resolve();
+        }
+
+        if (this.options.bgFill) {
+            ctx.fillStyle = this.options.bgFill;
+            let padding = this.options.bgPadding || 0;
+            ctx.fillRect(this.options.x - padding, this.options.y - padding, this.options.width + 2*padding, this.options.height + 2*padding);
+        }
+
+        let img = new Image(this.options.width, this.options.height);
+        if (this.href === 'bg-thumbnail.jpg') {
+            img.src = 'bg.png';
+        } else {
+            img.src = this.href;
+        }
+
+        return new Promise<void>((resolve, reject) => {
+            img.onload = () => {
+                let scale = Math.min(this.options.width / img.naturalWidth, this.options.height / img.naturalHeight);
+                let width = scale * img.naturalWidth;
+                let height = scale * img.naturalHeight;
+                let x = this.options.x + (this.options.width - width)/2;
+                let y = this.options.y + (this.options.height - height)/2;
+
+                ctx.drawImage(img, x, y, width, height);
+                resolve();
+            };
+            img.onerror = () => {
+                reject();
+            }
+        });
+    }
+}
+
+export interface Renderer<T> {
+    render(children: Array<RenderElement>): T
+}
+
+export class ReactRenderer implements Renderer<ReactNode> {
+    render(children: Array<RenderElement>): ReactNode {
+        let reactChildren: Array<ReactNode> = [];
+        for (let i = 0; i < children.length; i++) {
+            reactChildren.push(children[i].renderToReact(i));
+        }
+        return <svg width="100%" viewBox="0 0 1571 2000" children={reactChildren} />;
+    }
+}
+
+export class CanvasRenderer implements Renderer<Promise<HTMLCanvasElement>> {
+    render(children: Array<RenderElement>): Promise<HTMLCanvasElement> {
+        let canvas = document.createElement('canvas');
+        canvas.width = 1571;
+        canvas.height = 2000;
+
+        let chain: Promise<void> = Promise.resolve();
+        let ctx = canvas.getContext('2d');
+        ctx.textBaseline = 'bottom';
+        for (let i = 0; i < children.length; i++) {
+            let child = children[i];
+            chain = chain.then(() => child.renderToCanvas(ctx));
+        }
+        return chain.then(() => {
+            return canvas;
+        });
+    }
+}
