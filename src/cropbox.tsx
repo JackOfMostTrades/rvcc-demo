525d02cc3ff812de3ca99d9e8b58168e07eddc76
Ian Haken <ihaken@netflix.com>
2021-11-22 13:37:15 -0800

    Use OSS crop widget.

--- src/cropbox.tsx
+++ src/cropbox.tsx
@@ -1,37 +1,23 @@
-import {Component, MouseEvent} from "react";
-import {Button, Container, Dimmer, Image as SemanticImage, Loader, Modal} from "semantic-ui-react";
+import {Component} from "react";
+import {Button, Modal} from "semantic-ui-react";
+import ReactCrop from "react-image-crop";
 
 export interface Props {
     href: string;
     onComplete: (dataUrl: string) => void
 }
 
-enum Corner {
-    NONE,
-    TOP_LEFT,
-    TOP_RIGHT,
-    BOTTOM_LEFT,
-    BOTTOM_RIGHT
-}
-
 interface State {
-    loadedImage?: HTMLImageElement;
-    x?: number;
-    y?: number;
-    dx?: number;
-    dy?: number;
-    dragging?: Corner;
+    x?: number
+    y?: number
+    width?: number
+    height?: number
 }
 
 export class CropBox extends Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            x: 0,
-            y: 0,
-            dx: 1.0,
-            dy: 1.0,
-            dragging: Corner.NONE,
         };
     }
 
@@ -49,79 +35,28 @@ export class CropBox extends Component<Props, State> {
         this.setState({
             x: 0,
             y: 0,
-            dx: 1.0,
-            dy: 1.0,
-            dragging: Corner.NONE,
-            loadedImage: undefined,
+            width: 100,
+            height: 100,
         });
-
-        if (this.props.href) {
-            let img = new Image();
-            img.onload = () => {
-                if (img.src === this.props.href) {
-                    this.setState({
-                        loadedImage: img,
-                    });
-                }
-            };
-            img.src = this.props.href;
-        }
     }
 
-    private startDragging(corner: Corner) {
-        this.setState({dragging: corner});
-    }
-
-    private stopDragging() {
-        this.setState({dragging: Corner.NONE});
-    }
-
-    private doDrag(ev: MouseEvent<HTMLDivElement>) {
-        if (this.state.dragging === Corner.NONE) {
-            return false;
-        }
-        let rect = ev.currentTarget.getBoundingClientRect();
-        let x = (ev.clientX - rect.x) / rect.width;
-        let y = (ev.clientY - rect.y) / rect.height;
-        console.log("x=" + x + ", y=" + y);
-        if (this.state.dragging === Corner.TOP_LEFT) {
-            let newX = Math.min(this.state.dx, Math.max(x, 0));
-            let newY = Math.min(this.state.dy, Math.max(y, 0));
-            this.setState({x: newX, y: newY});
-        }
-        if (this.state.dragging === Corner.TOP_RIGHT) {
-            let newDX = Math.max(this.state.x, Math.min(x, 1.0));
-            let newY = Math.min(this.state.dy, Math.max(y, 0));
-            this.setState({dx: newDX, y: newY});
-        }
-        if (this.state.dragging === Corner.BOTTOM_LEFT) {
-            let newX = Math.min(this.state.dx, Math.max(x, 0));
-            let newDY = Math.max(this.state.y, Math.min(y, 1.0));
-            this.setState({x: newX, dy: newDY});
-        }
-        if (this.state.dragging === Corner.BOTTOM_RIGHT) {
-            let newDX = Math.max(this.state.x, Math.min(x, 1.0));
-            let newDY = Math.max(this.state.y, Math.min(y, 1.0));
-            this.setState({dx: newDX, dy: newDY});
-        }
-        return false;
-    }
 
     private doComplete() {
-        let img = this.state.loadedImage;
-        if (!img) {
-            throw new Error("Should only be reachable if image is loaded.");
+        let img = new Image();
+        img.onload = () => {
+            const x = (this.state.x || 0)/100.0 * img.naturalWidth;
+            const y = (this.state.y || 0)/100.0 * img.naturalHeight;
+            const width = (this.state.width || 100)/100.0 * img.naturalWidth;
+            const height = (this.state.height || 100)/100.0 * img.naturalHeight;
+            let canvas = document.createElement('canvas');
+            canvas.width = width;
+            canvas.height = height;
+            let ctx = canvas.getContext('2d');
+            ctx.drawImage(img, x, y, width, height,
+                0, 0, width, height);
+            this.props.onComplete(canvas.toDataURL('image/png'));
         }
-        let actualWidth = (this.state.dx-this.state.x) * img.naturalWidth;
-        let actualHeight = (this.state.dy-this.state.y) * img.naturalHeight;
-
-        let canvas = document.createElement('canvas');
-        canvas.width = actualWidth;
-        canvas.height = actualHeight;
-        let ctx = canvas.getContext('2d');
-        ctx.drawImage(img, this.state.x * img.naturalWidth, this.state.y * img.naturalHeight,
-            actualWidth, actualHeight, 0, 0, actualWidth, actualHeight);
-        this.props.onComplete(canvas.toDataURL('image/png'));
+        img.src = this.props.href;
     }
 
     render() {
@@ -130,38 +65,25 @@ export class CropBox extends Component<Props, State> {
             open={!!this.props.href}>
             <Modal.Header>Crop Your Image</Modal.Header>
             <Modal.Content>
-                {this.state.loadedImage ?
-                    <Container fluid textAlign="center" style={{msUserSelect: 'none', userSelect: 'none'}}>
-                        <div style={{width: "100%"}} onMouseUp={() => this.stopDragging()}>
-                            <div style={{position: 'relative', display: 'inline-block'}}
-                                 onMouseMove={(e) => this.doDrag(e)}>
-                                <SemanticImage style={{maxWidth: "100%", maxHeight: "300px", width: 'auto', height: 'auto'}} src={this.props.href} />
-                                <div style={{position: 'absolute', left: (this.state.x * 100.0).toString() + "%", top: (this.state.y * 100.0).toString() + "%",
-                                    borderTop: '2px solid blue', borderLeft: '2px solid blue', cursor: 'nwse-resize',
-                                    width: '20px', height: '20px'}}
-                                     onMouseDown={() => this.startDragging(Corner.TOP_LEFT)}
-                                />
-                                <div style={{position: 'absolute', right: ((1.0 - this.state.dx) * 100.0).toString() + "%", top: (this.state.y * 100.0).toString() + "%",
-                                    borderTop: '2px solid blue', borderRight: '2px solid blue', cursor: 'nesw-resize',
-                                    width: '20px', height: '20px'}}
-                                     onMouseDown={() => this.startDragging(Corner.TOP_RIGHT)}
-                                />
-                                <div style={{position: 'absolute', left: (this.state.x * 100.0).toString() + "%", bottom: ((1.0 - this.state.dy) * 100.0).toString() + "%",
-                                    borderBottom: '2px solid blue', borderLeft: '2px solid blue', cursor: 'nesw-resize',
-                                    width: '20px', height: '20px'}}
-                                     onMouseDown={() => this.startDragging(Corner.BOTTOM_LEFT)}
-                                />
-                                <div style={{position: 'absolute', right: ((1.0 - this.state.dx) * 100.0).toString() + "%", bottom: ((1.0 - this.state.dy) * 100.0).toString() + "%",
-                                    borderBottom: '2px solid blue', borderRight: '2px solid blue', cursor: 'nwse-resize',
-                                    width: '20px', height: '20px'}}
-                                     onMouseDown={() => this.startDragging(Corner.BOTTOM_RIGHT)}
-                                />
-                            </div>
-                        </div>
-                    </Container>
-                    :
-                    <Dimmer active inverted><Loader /></Dimmer>
-                }
+                <p>Drag to crop your image, or just click "Use this image" to continue.</p>
+                <ReactCrop
+                    src={this.props.href}
+                    imageStyle={{maxWidth: '300px', maxHeight: '300px', width: 'auto', height: 'auto'}}
+                    crop={{
+                        x: this.state.x,
+                        y: this.state.y,
+                        width: this.state.width,
+                        height: this.state.height,
+                        unit: '%',
+                    }}
+                    onChange={(crop, percentageCrop) => {
+                        this.setState({
+                            x: percentageCrop.x,
+                            y: percentageCrop.y,
+                            width: percentageCrop.width,
+                            height: percentageCrop.height,
+                        })
+                    }} />
             </Modal.Content>
             <Modal.Actions>
                 <Button color='black' onClick={() => this.props.onComplete("")}>
