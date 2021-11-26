e802dde3c32849e92a6dee420725b2711c859a2c
Ian Haken <ihaken@netflix.com>
2021-11-26 14:37:04 -0800

    Fixup ghpages script.

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -4,7 +4,8 @@ npm run build
 git branch -D gh-pages
 git checkout --orphan gh-pages
 git rm -r --cached .
-git add dist index.html blob-stream.js images/
+rm -f .gitignore
+git add dist/ index.html blob-stream.js images/
 git commit -m "Github Pages Build"
 git push --force -u origin gh-pages
 git reset --hard main
