886a6ad34e0487246d89abe25ed8a305fcbfb753
Ian Haken <ihaken@netflix.com>
2021-11-26 16:56:15 -0800

    Include rev in gh-pages commit.

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
+GITREV=$(git rev-parse --short HEAD)
 npm run build
 git branch -D gh-pages
 git checkout --orphan gh-pages
 git rm -r --cached .
 rm -f .gitignore
 git add dist/ index.html blob-stream.js images/
-git commit -m "Github Pages Build"
+git commit -m "Github Pages Build (${GITREV})"
 git push --force -u origin gh-pages
 git reset --hard main
 git checkout main
