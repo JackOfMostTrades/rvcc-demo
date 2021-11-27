0ec342a2a9b115205b4203db2c7ee686135b2d07
Ian Haken <ihaken@netflix.com>
2021-11-27 15:32:14 -0800

    Also build bingo pages.

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -2,11 +2,17 @@
 
 GITREV=$(git rev-parse --short HEAD)
 npm run build
+
+pushd bingo
+npm run build
+popd
+
 git branch -D gh-pages
 git checkout --orphan gh-pages
 git rm -r --cached .
 rm -f .gitignore
 git add dist/ index.html blob-stream.js images/
+git add bingo/dist/ bingo/index.html bingo/images/
 git commit -m "Github Pages Build (${GITREV})"
 git push --force -u origin gh-pages
 git reset --hard main
